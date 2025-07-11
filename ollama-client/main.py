5e1def167d22121bda9b072cbcfd058b81c6b805
Raphael Ramos <raphael.opensource@gmail.com>
2025-07-11 20:16:50 -0300

    docs: OllamaLLM construct parameters

--- ollama-client/main.py
+++ ollama-client/main.py
@@ -7,6 +7,9 @@ Answer: Let's think step by step."""
 
 prompt = ChatPromptTemplate.from_template(template)
 
+# TODO understand how to tune OllamaLLM constructor parameters
+# num_ctx, num_gpu, num_predict, num_thread, reasoning...
+# Reference: https://python.langchain.com/api_reference/ollama/llms/langchain_ollama.llms.OllamaLLM.html
 model = OllamaLLM(model="qwen3:1.7b", base_url="http://localhost:11434")
 
 chain = prompt | model
